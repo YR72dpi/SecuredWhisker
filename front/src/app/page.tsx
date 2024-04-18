e59aa8c07f9a9c255b65f31644aec4a67ed24676
Rousselle <ylan.rousselle76@gmail.com>
2024-04-18 19:22:19 +0200

    feat : init nextjs

---
+++ front/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <form id="registerForm">
+          <Input type="text" name="ip" placeholder="ip" />
+          <Input type="text" name="port" placeholder="port" />
+          <Input type="text" name="username" placeholder="username" />
+        </form>
+      </main>
+    </>
+  );
+}
