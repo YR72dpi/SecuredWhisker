199148dc9f0afabb51c481b82c924cac997a325d
Rousselle <ylan.rousselle76@gmail.com>
2024-04-24 14:21:08 +0200

    feat: style

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -5,10 +5,9 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(path.join(__dirname, '/src/styles'))
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, '/src/app/')],
+    includePaths: [path.join(__dirname, '/src/app/styles')],
   },
 };
 
