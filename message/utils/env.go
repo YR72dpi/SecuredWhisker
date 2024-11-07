7738224ca3e946810f8c29aa46ccb85f3a4582b8
Rousselle <ylan.rousselle76@gmail.com>
2024-11-07 20:38:15 +0100

    feat: possibility of configuring whether to show only error logs in the console

--- message/utils/env.go
+++ message/utils/env.go
@@ -8,29 +8,16 @@ import (
 )
 
 func LoadEnv() map[string]string {
-	env := os.Getenv("APP_ENV")
-	if env == "" || env == "dev" {
-		env = "dev"
-	}
 
-	err := godotenv.Load(".env." + env + ".local")
-	if err != nil {
-		log.Fatal("Error loading .env." + env + ".local file")
-	}
-
-	err = godotenv.Load("../.env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal("Error loading ../.env file")
 	}
 
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	return map[string]string{
-		"APP_ENV":        os.Getenv("APP_ENV"),
-		"REDIS_PASSWORD": os.Getenv("REDIS_PASSWORD"),
-		"VERSION":        os.Getenv("VERSION"),
+		"APP_ENV":         os.Getenv("APP_ENV"),
+		"REDIS_PASSWORD":  os.Getenv("REDIS_PASSWORD"),
+		"VERSION":         os.Getenv("VERSION"),
+		"SHOW_ONLY_ERROR": os.Getenv("SHOW_ONLY_ERROR"),
 	}
 }
--- message/utils/log.go
+++ message/utils/log.go
@@ -9,7 +9,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// CheckAndCreateDir vérifie si le dossier existe, sinon le crée
 func CheckAndCreateDir(dirPath string) {
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
 		err := os.Mkdir(dirPath, os.ModePerm)
@@ -19,62 +18,68 @@ func CheckAndCreateDir(dirPath string) {
 	}
 }
 
-// Logger écrit le log dans un fichier selon le niveau donné, avec logrus
-func Logger(level string, message string, writeInFile bool, data ...interface{}) {
+func Logger(level, message string, writeInFile bool, data ...interface{}) {
 	level = strings.ToLower(level)
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	logMessage := fmt.Sprintf("[%s][%s] %s", timestamp, level, message)
 
-	if len(data) != 0 {
-		fmt.Println("["+time.Now().Format("2006-01-02 15:04:05")+"]"+"["+level+"] "+message, data)
-	} else {
-		fmt.Println("[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + level + "] " + message)
+	// Vérifie si l'affichage en console est nécessaire en fonction de l'environnement et du niveau de log
+	if !(LoadEnv()["SHOW_ONLY_ERROR"] == "true" && (level == "info" || level == "print")) {
+		if len(data) != 0 {
+			fmt.Println(logMessage, data)
+		} else {
+			fmt.Println(logMessage)
+		}
 	}
 
+	// Ecrit le log dans un fichier si nécessaire
 	if writeInFile {
+		writeLogToFile(level, message, data)
+	}
+}
 
-		CheckAndCreateDir("./log")
-		fileName := "./log/" + level + ".log"
-		dataStr := fmt.Sprintf("["+level+"] %v", data)
+// Fonction utilitaire pour écrire le log dans un fichier
+func writeLogToFile(level, message string, data []interface{}) {
+	CheckAndCreateDir("./log")
+	fileName := fmt.Sprintf("./log/%s.log", level)
 
-		logEntry := LogEntry{
-			Level:   level,
-			Message: message,
-			Data:    dataStr,
-			Time:    time.Now().Format(time.RFC3339),
-		}
-
-		// Initialiser le logger
-		logger := logrus.New()
-		logger.SetFormatter(&logrus.JSONFormatter{})
-
-		// Configurer la sortie du logger vers le fichier
-		file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-			fmt.Println("Erreur d'ouverture du fichier :", err)
-			return
-		}
-
-		defer file.Close()
-		logger.SetOutput(file)
-
-		// Définir le niveau de log en fonction du paramètre level
-		switch level {
-		case "info":
-			logger.WithFields(logrus.Fields{
-				"data": logEntry.Data,
-			}).Info(logEntry.Message)
-		case "error":
-			logger.WithFields(logrus.Fields{
-				"data": logEntry.Data,
-			}).Error(logEntry.Message)
-		case "warn":
-			logger.WithFields(logrus.Fields{
-				"data": logEntry.Data,
-			}).Warn(logEntry.Message)
-		default:
-			logger.WithFields(logrus.Fields{
-				"data": logEntry.Data,
-			}).Print(logEntry.Message)
-		}
+	// Formatage des données supplémentaires en chaîne
+	dataStr := fmt.Sprintf("%v", data)
+	logEntry := LogEntry{
+		Level:   level,
+		Message: message,
+		Data:    dataStr,
+		Time:    time.Now().Format(time.RFC3339),
 	}
 
+	// Initialiser le logger
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	// Configurer la sortie du logger vers le fichier
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Erreur d'ouverture du fichier :", err)
+		return
+	}
+	defer file.Close()
+	logger.SetOutput(file)
+
+	// Écrit le message dans le fichier en fonction du niveau
+	logWithLevel(logger, level, logEntry)
+}
+
+// Fonction utilitaire pour définir le niveau de log et écrire le message
+func logWithLevel(logger *logrus.Logger, level string, entry LogEntry) {
+	fields := logrus.Fields{"data": entry.Data}
+	switch level {
+	case "info":
+		logger.WithFields(fields).Info(entry.Message)
+	case "error":
+		logger.WithFields(fields).Error(entry.Message)
+	case "warn":
+		logger.WithFields(fields).Warn(entry.Message)
+	default:
+		logger.WithFields(fields).Print(entry.Message)
+	}
 }
