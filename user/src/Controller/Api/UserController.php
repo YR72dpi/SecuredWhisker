653c3d2fa0379f6d527a9d1187c55964496b0613
Rousselle <ylan.rousselle76@gmail.com>
2024-12-23 22:10:03 +0100

    refactor: file management

--- user/src/Controller/Api/UserController.php
+++ user/src/Controller/Api/UserController.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/api/user', name: 'api_user_')]
 class UserController extends AbstractController
@@ -46,7 +47,7 @@ class UserController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
         
-        return new JsonResponse(['message' => 'User registered successfully'], 201);
+        return new JsonResponse(['message' => 'User registered successfully'], Response::HTTP_CREATED);
     }
 
     #[Route('/publicKey/{userId}', name: 'publicKey')]
@@ -57,10 +58,12 @@ class UserController extends AbstractController
     {
         $userData = $userRepository->findOneBy(["id" => $userId]);
 
+        if(!$userData) return new JSONResponse(["message" => "User public key not find"]);
+        
         return new JsonResponse([
             'id' => $userData->getId(),
             'username' => $userData->getUsername(),
             'publicKey' => $userData->getPublicKey(),
-        ], 200);
+        ], Response::HTTP_OK);
     }
 }
