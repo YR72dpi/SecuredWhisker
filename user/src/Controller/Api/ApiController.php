02c6c78e295ae1721e4a922a0214922209de6975
Rousselle <ylan.rousselle76@gmail.com>
2024-12-23 22:38:17 +0100

    feat: cache on api publicKey

--- user/src/Controller/Api/ApiController.php
+++ user/src/Controller/Api/ApiController.php
@@ -4,60 +4,68 @@ namespace App\Controller\Api;
 
 use App\Kernel;
 use phpseclib3\Crypt\RSA;
-use phpseclib3\File\ASN1\Maps\RSAPublicKey;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 #[Route('/api', name: 'api_')]
 class ApiController extends AbstractController
 {
     #[Route('/publicKey', name: 'publicKey')]
     public function getServerPublicKey(
-        Kernel $kernel
-    ): JsonResponse
-    {
-        $publicKeyPath = 
+        Kernel $kernel,
+        CacheInterface $cache
+    ): JsonResponse {
+        $publicKeyPath =
             $kernel->getProjectDir()
             . DIRECTORY_SEPARATOR
             . "config"
             . DIRECTORY_SEPARATOR
             . "keys"
             . DIRECTORY_SEPARATOR
-            . "publicKey.key"
-        ;
+            . "publicKey.key";
 
-        $privateKeyPath = 
+        $privateKeyPath =
             $kernel->getProjectDir()
             . DIRECTORY_SEPARATOR
             . "config"
             . DIRECTORY_SEPARATOR
             . "keys"
             . DIRECTORY_SEPARATOR
-            . "privateKey.key"
-        ;
+            . "privateKey.key";
 
-        if(!file_exists($publicKeyPath)) return new JsonResponse([
-            "message" => "Public key file not found"
-        ], Response::HTTP_NOT_FOUND);
+        return $cache->get(
+            md5("api_publicKey" . $publicKeyPath . $privateKeyPath),
+            function (ItemInterface $item) use ($publicKeyPath, $privateKeyPath) {
+                $item->expiresAfter(5 * 60);
 
-        $publicKey = file_get_contents($publicKeyPath);
+                if (!file_exists($publicKeyPath)) return new JsonResponse([
+                    "message" => "Public key file not found"
+                ], Response::HTTP_NOT_FOUND);
 
-        $message = "A message for test";
-        $private = RSA::loadPrivateKey(file_get_contents($privateKeyPath));
-        $signature = $private->sign($message);
-        $isSignatureValid = $private->getPublicKey()->verify($message, $signature) ?
-            true :
-            false;
+                $publicKey = file_get_contents($publicKeyPath);
 
-        if(!$isSignatureValid) return new JsonResponse([
-            "RSA keys invalid"
-        ], Response::HTTP_CONFLICT);
+                $message = uniqid();
+                try {
+                    $private = RSA::loadPrivateKey(file_get_contents($privateKeyPath));
+                    $signature = $private->sign($message);
+                    $isSignatureValid = $private->getPublicKey()->verify($message, $signature) ?
+                        true :
+                        false;
+                } catch (\Throwable $th) {
+                    return new JsonResponse([
+                        "message" => "RSA keys invalid"
+                    ], Response::HTTP_CONFLICT);
+                }
 
-        return new JsonResponse([
-            "message" => "ok",
-            "publicKey" => base64_encode($publicKey)
-        ], Response::HTTP_OK);
+                return new JsonResponse([
+                    "message" => "ok",
+                    "publicKey" => base64_encode($publicKey)
+                ], Response::HTTP_OK);
+            }
+        );
     }
 }
