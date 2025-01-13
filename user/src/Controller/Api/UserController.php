f5ef97cecc9db5875a0113a1a7a4841aded11851
Rousselle <ylan.rousselle76@gmail.com>
2025-01-13 21:28:07 +0100

    feat: unit test donne

--- user/src/Controller/Api/UserController.php
+++ user/src/Controller/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Api;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,51 +16,61 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 #[Route('/api/user', name: 'api_user_')]
 class UserController extends AbstractController
 {
-    #[Route('/subscribe', name: 'subscribe')]
+    #[Route('/subscribe', name: 'subscribe', methods: ["POST"])]
     public function index(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasherInterface,
-        EntityManagerInterface $entityManager
-    ): JsonResponse
-    {
+        EntityManagerInterface $entityManager,
+        UserRepository $userRepository,
+        LoggerInterface $logger
+    ): JsonResponse {
         $data = json_decode($request->getContent(), true);
 
-        if (
-            empty($data["username"])
-            || empty($data["password"])
-            || empty($data["publicKey"])
-        ) {
-            return new JsonResponse(['error' => 'Username, password, public key is missing'], 400);
-        }
+        if (empty($data["username"]))
+            return new JsonResponse(['error' => 'Username is missing'], Response::HTTP_BAD_REQUEST);
+        if (empty($data["password"]))
+            return new JsonResponse(['error' => 'Password is missing'], Response::HTTP_BAD_REQUEST);
+        if (empty($data["publicKey"]))
+            return new JsonResponse(['error' => 'Public key is missing'], Response::HTTP_BAD_REQUEST);
 
         $username = htmlentities($data["username"]);
         $password = htmlentities($data["password"]);
-        $publicKey= htmlentities($data["publicKey"]);
+        $publicKey = htmlentities($data["publicKey"]);
 
-        $user = new User();
-        $user->setUsername($username);
-         
-        $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $password);
+        if ($userRepository->findOneBy(["username" => $username]))
+            return new JsonResponse([
+                'message' => 'User already exists'
+            ], Response::HTTP_CONFLICT);
 
-        $user->setPassword($hashedPassword);
-        $user->setPublicKey($publicKey);
+        try {
+            $user = new User();
+            $user->setUsername($username);
+    
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $password);
+    
+            $user->setPassword($hashedPassword);
+            $user->setPublicKey($publicKey);
+    
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-        $entityManager->persist($user);
-        $entityManager->flush();
-        
-        return new JsonResponse(['message' => 'User registered successfully'], Response::HTTP_CREATED);
+            return new JsonResponse(['message' => 'User registered successfully'], Response::HTTP_CREATED);
+        } catch(\Throwable $err) {
+            $logger->error("Error while saving subscribe user", ["exception" => $err->getMessage()]);
+            return new JsonResponse(['message' => 'Something went wrong'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     #[Route('/publicKey/{userId}', name: 'publicKey')]
     public function getPublicKey(
         int $userId,
         UserRepository $userRepository
-    ): JsonResponse
-    {
+    ): JsonResponse {
         $userData = $userRepository->findOneBy(["id" => $userId]);
 
-        if(!$userData) return new JSONResponse(["message" => "User public key not find"]);
-        
+        if (!$userData) return new JSONResponse(["message" => "User public key not find"], Response::HTTP_NOT_FOUND);
+
         return new JsonResponse([
             'id' => $userData->getId(),
             'username' => $userData->getUsername(),
--- user/tests/UserTest.php
+++ user/tests/UserTest.php
@@ -3,9 +3,28 @@
 namespace App\Tests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserTest extends ApiTestCase
 {
+    private EntityManagerInterface $entityManager;
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        // Boot the Symfony kernel
+        $kernel = self::bootKernel();
+
+        // Get the EntityManager from the service container
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+        $this->userRepository = $this->entityManager->getRepository(User::class);
+            
+    }
+
     public function testIndex(): void
     {
         $response = static::createClient()->request('GET', '/');
@@ -17,25 +36,100 @@ class UserTest extends ApiTestCase
         ]);
     }
 
-    // public function addUser(): void
-    // {
-    //     $response = static::createClient()->request('GET', '/');
+    public function testSubscribeSuccess()
+    {
+        $client = static::createClient();
 
-    //     $this->assertResponseIsSuccessful();
-    //     $this->assertJsonContains([
-    //         'message' => "That's Secured Whisker's user micro service",
-    //         'status' => 200
-    //     ]);
-    // }
+        $data = json_encode([
+            'username' => 'testuser',
+            'password' => 'securepassword',
+            'publicKey' => 'testPublicKey123'
+        ]);
 
-    // public function checkUserPublicKey(): void
-    // {
-    //     $response = static::createClient()->request('GET', '/');
+        $client->request(
+            'POST',
+            '/api/user/subscribe',
+            ["body" => $data],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+        );
 
-    //     $this->assertResponseIsSuccessful();
-    //     $this->assertJsonContains([
-    //         'message' => "That's Secured Whisker's user micro service",
-    //         'status' => 200
-    //     ]);
-    // }
+        $this->assertResponseStatusCodeSame(201);
+
+        $responseContent = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('User registered successfully', $responseContent['message']);
+
+        $foundUser = $this->userRepository->findOneBy(['username' => 'testuser']);
+
+        // Vérifier que l'utilisateur est bien trouvé
+        $this->assertNotNull($foundUser);
+        $this->assertSame('testPublicKey123', $foundUser->getPublicKey());
+    }
+
+    public function testSubscribeSameUser()
+    {
+        $client = static::createClient();
+
+        $data = json_encode([
+            'username' => 'testuser',
+            'password' => 'securepassword',
+            'publicKey' => 'testPublicKey123'
+        ]);
+
+        $client->request(
+            'POST',
+            '/api/user/subscribe',
+            ["body" => $data],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+        );
+
+        $this->assertResponseStatusCodeSame(409);
+    }
+
+    
+    public function checkUserPublicKey(): void
+    {
+        $userTest = $this->userRepository->findOneBy(["username" => "testuser"]);
+
+        $response = static::createClient()->request('GET', '/api/user/publicKey/'.$userTest->getId());
+
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseIsSuccessful();
+    }
+
+    
+    public function testRemoveUser()
+    {
+       try { 
+            $userTest = $this->userRepository->findOneBy(["username" => "testuser"]);
+            $this->assertNotNull($userTest);
+            $this->entityManager->remove($userTest);
+            $this->entityManager->flush();
+            $this->assertIsBool(true);
+            
+        } catch (\Throwable $err) {
+            $this->fail("Remove user with entity manager: ".$err->getMessage());
+       }
+    }
+
+    public function testSubscribeValidationError()
+    {
+        $client = static::createClient();
+
+        $data = json_encode([
+            'username' => 'testFakeTwinUser',
+            'password' => 'verysecuredpassword'
+        ]);
+
+        $client->request(
+            'POST',
+            '/api/user/subscribe',
+            ["body" => $data],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+        );
+
+        $this->assertResponseStatusCodeSame(400);
+    }
 }
