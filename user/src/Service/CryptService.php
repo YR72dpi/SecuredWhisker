0539f9f79200c130df5c86d2879804f387cc19fb
Rousselle <ylan.rousselle76@gmail.com>
2025-01-27 21:32:23 +0100

    feat: crypt service on user php side

---
+++ user/src/Service/CryptService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use App\Kernel;
+use phpseclib3\Crypt\Common\AsymmetricKey;
+use phpseclib3\Crypt\RSA;
+
+class Crypt
+{
+    private string $privateKey;
+
+
+    public function __construct(Private Kernel $kernel)
+    {
+        $configPath = $this->kernel->getProjectDir() . DIRECTORY_SEPARATOR . "config";
+
+        $privateKeyFile = 
+            $configPath 
+            .  DIRECTORY_SEPARATOR
+            . "keys"
+            . DIRECTORY_SEPARATOR
+            . "privateKey.key";
+        
+        $this->privateKey = file_get_contents($privateKeyFile);
+    }
+
+    public function decrypt(string $encryptedText): string
+    {
+        // Charger la clé privée
+        $privateKey = $this->privateKey;
+        openssl_private_decrypt(
+            base64_decode($encryptedText),
+            $decryptedMessage,
+            $privateKey,
+            OPENSSL_PKCS1_OAEP_PADDING
+        );
+    
+        return $decryptedMessage;
+    }
+}
