ee449275bde7cbb2de1f820f9256186d1f8181aa
Ylan <ylan.rousselle@gmail.com>
2025-06-26 13:38:02 +0000

    fix: probleme on prod with the ../env

--- message/utils/env.go
+++ message/utils/env.go
@@ -1,23 +1,23 @@
 package utils
 
 import (
-	"log"
+	// "log"
 	"os"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func LoadEnv() map[string]string {
 
 	// TODO : 2025/06/24 20:13:06 Error loading ../.env file on docker
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Fatal("Error loading ../.env file")
-	}
+	// err := godotenv.Load("../../.env")
+	// if err != nil {
+		// log.Fatal("Error loading ../../.env file")
+	// }
 
 	return map[string]string{
-		"APP_ENV":         os.Getenv("APP_ENV"),
-		"VERSION":         os.Getenv("VERSION"),
+		// "APP_ENV":         os.Getenv("APP_ENV"),
+		// "VERSION":         os.Getenv("VERSION"),
 		"SHOW_ONLY_ERROR": os.Getenv("SHOW_ONLY_ERROR"),
 	}
 }
