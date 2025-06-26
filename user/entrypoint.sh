370751489e55b7bf6de25c65999ef0fb96b5366f
Ylan <ylan.rousselle@gmail.com>
2025-06-26 08:46:24 +0000

    generate jwt keys pair

--- user/entrypoint.sh
+++ user/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
-php bin/console doctrine:migrations:migrate --no-interaction || true
-php bin/console doctrine:migration:diff --no-interaction || true
-php bin/console app:generate-rsa-keys || true
+echo "Entrypoint script started..."
+echo "Running migrations..."
+php bin/console doctrine:migration:diff --no-interaction
+php bin/console doctrine:migrations:migrate --no-interaction
+echo "Generating JWT keys..."
+php bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction
+echo "Generating RSA keys..."
+php bin/console app:generate-rsa-keys
 
 exec "$@"
