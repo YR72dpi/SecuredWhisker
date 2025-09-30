0121719daefa93a1a3ef5a5e6aa6753dc0b82804
Rousselle <ylan.rousselle76@gmail.com>
2025-09-30 20:15:09 +0200

    feat: responsive contact list

--- front/components/contactList.tsx
+++ front/components/contactList.tsx
@@ -54,38 +54,40 @@ export function ContactList({ onSelectContact, refreshKey }: ContactListProps) {
     }, [refreshKey])
 
     return (
-        <>
-            {isLoading ? (<p>Loading....</p>) : ""}
-            {error !== "" && <p className="text-red-500">{error}</p>}
-            {!isLoading && error === "" && (
-                contacts.length === 0 ? (
-                    <p>No contact</p>
-                ) : (
-                    <>
-                        <span className="font-bold text-xl">Contacts ({contacts === null ? "Loading..." : contacts.length})</span><ul className="mt-2 space-y-2">
-                            {contacts.map((contact, index) => (
-                                <li
-                                    key={index}
-                                    className="border-b p break-all max-w-[400px]"
-                                    onClick={() => {
-                                        onSelectContact(
-                                            {
-                                                id: contact.id,
-                                                username: contact.username,
-                                                uniqid: contact.uniqid,
-                                                publicKey: contact.publicKey
-                                            }
-                                        );
-                                    }}
-                                >
-                                    {contact.username ?? "Unnamed contact"}
-                                    <span className="text-sm text-gray-500 italic"> {contact.uniqid ? " (" + contact.uniqid + ")" : ""}</span>
-                                </li>
-                            ))}
-                        </ul>
-                    </>
+            <>
+                {isLoading ? (<p>Loading....</p>) : ""}
+                {error !== "" && <p className="text-red-500">{error}</p>}
+                {!isLoading && error === "" && (
+                    contacts.length === 0 ? (
+                        <p>No contact</p>
+                    ) : (
+                        <>
+                            <span className="font-bold text-xl text-center">Contacts ({contacts === null ? "Loading..." : contacts.length})</span>
+                            <ul className="mt-2 space-y-2 flex flex-col items-center">
+                                {contacts.map((contact, index) => (
+                                    <li
+                                        key={index}
+                                        className="border-b p break-all text-center w-[300px] max-w-[90%]"
+                                        onClick={() => {
+                                            onSelectContact(
+                                                {
+                                                    id: contact.id,
+                                                    username: contact.username,
+                                                    uniqid: contact.uniqid,
+                                                    publicKey: contact.publicKey
+                                                }
+                                            );
+                                        }}
+                                    >
+                                        {contact.username ?? "Unnamed contact"}
+                                        <span className="text-sm text-gray-500 italic"> {contact.uniqid ? " (" + contact.uniqid + ")" : ""}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </>
+                    )
                 )
-            )}
+            }
         </>
     )
 }
