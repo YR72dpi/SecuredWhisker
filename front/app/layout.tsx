acc2c0807391d96c72036e27ba6d4de4433abfd0
Rousselle <ylan.rousselle76@gmail.com>
2025-10-16 21:37:13 +0200

    feat: enhance layout metadata and viewport settings for improved PWA support

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,8 +1,53 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import { useEffect } from "react";
+
+const APP_NAME = "Secure Whisker";
+const APP_DEFAULT_TITLE = "Secured Whisker";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Free et secured chat platform";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
+};
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,35 +61,31 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "Secured Whisker",
-  description: "The secured and open source chat",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/icons/favicon.ico" />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
 
-          {children}
-          <Toaster richColors />
-        </ThemeProvider>
-      </body>
-    </html>
+  return (
+      <html lang="en" suppressHydrationWarning dir="ltr">
+        <head>
+          <link rel="icon" href="/icons/favicon.ico" />
+        </head>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+
+            {children}
+            <Toaster richColors />
+          </ThemeProvider>
+        </body>
+      </html>
   );
 }
