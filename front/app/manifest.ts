0079e113bebb0d9972c910b3b90a40f0e085448e
Rousselle <ylan.rousselle76@gmail.com>
2025-10-25 01:38:21 +0200

    feat: better manifest

--- front/app/manifest.ts
+++ front/app/manifest.ts
@@ -2,15 +2,30 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: "SecWhis",
     name: 'Secured Whisker',
     short_name: 'SecWhis',
     description: 'The secured and open source chat',
+    lang: "en",
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
     orientation: 'portrait',
     theme_color: '#ffffff',
     categories: ['social', 'chat'],
+    "display_override" : [
+      "standalone",
+        "minimal-ui",
+        "fullscreen",
+        "window-controls-overlay"
+    ],
+    shortcuts: [
+        {
+            "name": "Notification manager",
+            "url": "/notification",
+            "description": "Check and delete devices that can be notified"
+        }
+    ],
     icons: [
       {
         src: '/icons/icon-192x192.png',
